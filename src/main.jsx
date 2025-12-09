b78c1742e205f562f599537a0ba62d79d43fcc02
vaa1cr911-rgb <vaa1cr911@gmail.com>
2025-12-09 09:51:22 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
-// src/main.jsx
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App';
+import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(<App />);
